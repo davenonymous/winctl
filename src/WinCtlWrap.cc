857954a7fc81b86ea29d9e2079ab7a4eba7823c5
Christian Hoffmann <chris.hoffmann84@gmail.com>
2022-09-15 15:27:35 +0200

    make winctl compatible with electron v20 and above

--- src/WinCtlWrap.cc
+++ src/WinCtlWrap.cc
@@ -5,7 +5,7 @@ using v8::FunctionTemplate;
 void InitAll(v8::Local<v8::Object> exports) {
 	Window::Init(exports);
 
-	v8::Local<v8::Context> context = exports->CreationContext();
+	v8::Local<v8::Context> context = exports->GetCreationContext().ToLocalChecked();
 	exports->Set(context, Nan::New("GetActiveWindow").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(Window::GetActiveWindow)->GetFunction(context).ToLocalChecked());
 	exports->Set(context, Nan::New("GetWindowByClassName").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(Window::GetWindowByClassName)->GetFunction(context).ToLocalChecked());
 	exports->Set(context, Nan::New("GetWindowByTitleExact").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(Window::GetWindowByTitleExact)->GetFunction(context).ToLocalChecked());
